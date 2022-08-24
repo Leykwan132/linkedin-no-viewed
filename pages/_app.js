95114077568d1aa08bf97b81431b8b3ccfce5fed
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-24 20:52:14 +0800

    Adding Recoil and Toaster.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <RecoilRoot>
+        <Toaster />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
