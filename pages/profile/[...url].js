1382db3105a841b5e84a166bc2362c1676c09478
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-25 18:22:07 +0800

    First version of dynamic canvas content.

--- pages/profile/[...url].js
+++ pages/profile/[...url].js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { useSetRecoilState, useRecoilValue } from "recoil";
-import { profileState } from "../../atoms/profileAtoms.ts";
+import { profileState, canvasState } from "../../atoms/profileAtoms.ts";
 import FloatingButton from "../../components/FloatingButton.tsx";
 import Link from "next/link";
 
@@ -20,6 +20,8 @@ const arrayTitle = [
 ];
 const LinkedinProfile = () => {
   const profileData = useRecoilValue(profileState);
+  const canvasData = useRecoilValue(canvasState);
+
   console.log(profileData);
   return (
     <div className="flex flex-col h-screen pt-24 px-20">
@@ -33,30 +35,34 @@ const LinkedinProfile = () => {
           />
         </div>
         <div className="flex flex-col h-[300px] w-[600px] border border-gray-400 rounded-2xl py-2 px-6 items-center space-y-4 justify-center fadeRightMini">
-          <div className="text-gray-300 font-mono font-bold text-3xl">
-            {profileData.full_name}
-          </div>
-          <div className="text-gray-300 font-mono font-semibold">
-            {`${profileData?.city}, ${profileData?.country_full_name}`}
-          </div>
-          <p className="text-gray-300 font-mono font-semibold max-w-[500px] text-center">
-            {`${profileData?.Education[0]?.degree_name}, ${profileData?.Education[0]?.field_of_study}`}
-          </p>
-          <div className="flex items-center space-x-2">
-            <Image
-              src={profileData?.Education[0]?.logo_url}
-              className="rounded-full"
-              objectFit="contain"
-              width="25px"
-              height="25px"
-            />
-            <p className="text-gray-300 font-mono font-semibold text-center">
-              {profileData?.Education[0].school}
-            </p>
-          </div>
-          <p className="text-gray-300 font-mono font-semibold text-center">
-            {`Current ${profileData?.headline}`}
-          </p>
+          {canvasData === "Bio" && (
+            <>
+              <div className="text-gray-300 font-mono font-bold text-3xl">
+                {profileData.full_name}
+              </div>
+              <div className="text-gray-300 font-mono font-semibold">
+                {`${profileData?.city}, ${profileData?.country_full_name}`}
+              </div>
+              <p className="text-gray-300 font-mono font-semibold max-w-[500px] text-center">
+                {`${profileData?.Education[0]?.degree_name}, ${profileData?.Education[0]?.field_of_study}`}
+              </p>
+              <div className="flex items-center space-x-2">
+                <Image
+                  src={profileData?.Education[0]?.logo_url}
+                  className="rounded-full flex-shrink-0"
+                  objectFit="contain"
+                  width="25px"
+                  height="25px"
+                />
+                <p className="text-gray-300 font-mono font-semibold text-center">
+                  {profileData?.Education[0].school}
+                </p>
+              </div>
+              <p className="text-gray-300 font-mono font-semibold text-center">
+                {`Current ${profileData?.headline}`}
+              </p>
+            </>
+          )}
         </div>
       </div>
       <div className="mt-20 grid grid-cols-5 gap-10 fade">
