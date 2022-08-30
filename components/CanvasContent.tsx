b15fa4caefe38aa9248c1f611af7a2da97fe43ae
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-30 15:49:04 +0800

    Interface added for the canvas Content.

--- components/CanvasContent.tsx
+++ components/CanvasContent.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { canvasState, profileState } from "../atoms/profileAtoms.ts";
 import EduContent from "./CanvasContent/EduContent.tsx";
+import ExperienceContent from "./CanvasContent/ExperienceContent.tsx";
 import LanguageContent from "./CanvasContent/LanguageContent.tsx";
 
 type Props = {};
 
-const contentType = {
+interface Content {
+  Education?: any;
+  Languages?: any;
+  Experiences?: any;
+  Patents?: any;
+  Certs?: any;
+  Projects?: any;
+  Test_Scores?: any;
+  Volunteering?: any;
+  Publications?: any;
+  Courses?: any;
+}
+const contentType: Content = {
   Education: <EduContent />,
   Languages: <LanguageContent />,
+  Experiences: <ExperienceContent />,
 };
 const CanvasContent = (props: Props) => {
   const profileData = useRecoilValue(profileState);
