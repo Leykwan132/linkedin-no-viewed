38a8925ad71b4c2ce81d8895f1f7fedf2dc46e4c
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-30 17:09:36 +0800

    Test score content implementation.

---
+++ components/CanvasContent/TestScore.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { canvasState, profileState } from "../../atoms/profileAtoms.ts";
+import TestScoresRow from "./components/TestScoresRow.tsx";
+import { SiPytest } from "react-icons/si";
+import { GiPapers } from "react-icons/gi";
+import { TbPaperBag } from "react-icons/tb";
+type Props = {};
+
+const Emoji = {
+  1: <SiPytest />,
+  2: <GiPapers />,
+  3: <TbPaperBag />,
+};
+const TestScore = (props: Props) => {
+  const profileData = useRecoilValue(profileState);
+  const canvasData = useRecoilValue(canvasState);
+  const testScoreData = profileData[canvasData];
+  const counter = [0, 1, 2];
+
+  return (
+    <div className="space-y-6 fade w-full">
+      {testScoreData.length <= 2
+        ? testScoreData.map((item, i) => (
+            <TestScoresRow key={i} data={item} emoji={Emoji[i]} />
+          ))
+        : counter.map((item, i) => (
+            <TestScoresRow
+              key={i}
+              data={testScoreData[item]}
+              emoji={Emoji[item]}
+            />
+          ))}
+    </div>
+  );
+};
+
+export default TestScore;
---
+++ components/CanvasContent/components/TestScoresRow.tsx
@@ -0,0 +1,20 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+
+type Props = {
+  data?: Object;
+  emoji?: any;
+};
+
+const TestScoresRow = ({ data, emoji }: Props) => {
+  console.log(data);
+  return (
+    <div className="font-mono text-white flex flex-col  space-y-3  px-2">
+      <p className="font-bold">{data.name}</p>
+
+      <p className="text-xs font-thin">{`Score: ${data.score}`}</p>
+    </div>
+  );
+};
+
+export default TestScoresRow;
