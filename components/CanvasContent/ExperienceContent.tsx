327a3a467764b60ef325ae333c9a41ba6e359e53
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-30 17:08:47 +0800

    Experience limited to only 3 item.

--- components/CanvasContent/ExperienceContent.tsx
+++ components/CanvasContent/ExperienceContent.tsx
@@ -9,10 +9,11 @@ const ExperienceContent = (props: Props) => {
   const profileData = useRecoilValue(profileState);
   const canvasData = useRecoilValue(canvasState);
   const expData = profileData[canvasData];
+  const counter = [0, 1, 2];
   return (
     <div className="space-y-6 fade w-full">
-      {expData.map((item, i) => (
-        <ExperienceContentRow key={i} data={item} />
+      {counter.map((item, i) => (
+        <ExperienceContentRow key={i} data={expData[item]} />
       ))}
     </div>
   );
