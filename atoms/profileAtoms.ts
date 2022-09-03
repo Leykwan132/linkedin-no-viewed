825ef3c1fbd2e2899e1cda017524145996d0aa82
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-04 01:28:23 +0300

    recoil persist removed.

--- atoms/profileAtoms.ts
+++ atoms/profileAtoms.ts
@@ -1,24 +1,19 @@
 import React from "react";
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist();
 type Props = {};
 
 export const profileState = atom({
   key: "profile", // unique ID (with respect to other atoms/selectors)
   default: {}, // default value (aka initial value)
-  effects_UNSTABLE: [persistAtom],
 });
 
 export const canvasState = atom({
   key: "canvas", // unique ID (with respect to other atoms/selectors)
   default: "Bio", // default value (aka initial value)
-  effects_UNSTABLE: [persistAtom],
 });
 
 export const linkedinState = atom({
   key: "linkedin", // unique ID (with respect to other atoms/selectors)
   default: "", // default value (aka initial value)
-  effects_UNSTABLE: [persistAtom],
 });
