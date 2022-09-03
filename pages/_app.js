d2a6fc17c98ae67a5295f4b6e2145028541f8e65
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-04 01:28:44 +0300

    Header component added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import React, { useEffect } from "react";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+import { RecoilRoot } from "recoil";
+import Header from "../components/Header.tsx";
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const use = async () => {
@@ -20,6 +16,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <RecoilRoot>
         <Toaster />
+        <Header />
         <Component {...pageProps} />
       </RecoilRoot>
     </>
