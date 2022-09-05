7dab26e430ba52c3026bbb08bc7e89fe72e8cb8e
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-04 22:02:42 -0500

    First version Mobile UI.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiFillGithub, AiOutlineHome, AiFillLinkedin } from "react-icons/ai";
 import Link from "next/link";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 
 type Props = {};
 
@@ -8,20 +9,20 @@ const Header = (props: Props) => {
   return (
     <div className="relative">
       <Link href={"/"}>
-        <AiOutlineHome className="absolute left-0 w-7 h-7 mt-5 ml-7 cursor-pointer z-10 hover:text-gray-400" />
+        <AiOutlineHome className="absolute left-0 w-7 h-7 mt-7 ml-7 cursor-pointer z-10 hover:text-gray-400" />
       </Link>
       <div className="flex items-center">
         <a
           href="https://www.linkedin.com/in/ley-kwan-choo-129678228/"
           target={"_blank"}
         >
-          <AiFillLinkedin className="absolute right-12 w-7 h-7 mt-5 mr-7 cursor-pointer z-10 hover:text-gray-400" />
+          <AiFillLinkedin className="absolute right-12 w-7 h-7 mt-7 mr-7 cursor-pointer z-10 hover:text-gray-400" />
         </a>
         <a
           href="https://github.com/Leykwan132/linkedin-no-viewed"
           target={"_blank"}
         >
-          <AiFillGithub className="absolute right-0 w-7 h-7 mt-5 mr-7 cursor-pointer z-10 hover:text-gray-400" />
+          <AiFillGithub className="absolute right-0 w-7 h-7 mt-7 mr-7 cursor-pointer z-10 hover:text-gray-400" />
         </a>
       </div>
     </div>
