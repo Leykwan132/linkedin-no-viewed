a9d792bc34172a0254c2b67e7d7a05b39f9a3685
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-06 05:50:49 -0500

    Cert Mobile version implemented.

--- components/CanvasContent/components/CertContextRow.tsx
+++ components/CanvasContent/components/CertContextRow.tsx
@@ -1,5 +1,7 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import { useRecoilState } from "recoil";
+import { isMobileState } from "../../../atoms/profileAtoms.ts";
 import { dateRangeFormatter } from "../../../utils/dateRangeFormatter.ts";
 
 type Props = {
@@ -10,7 +12,7 @@ const CertContextRow = ({ data }: Props) => {
   const start_date = dateRangeFormatter(data, true);
 
   return (
-    <div className="font-mono flex flex-col text-[15px] space-y-3">
+    <div className="font-mono flex flex-col md:text-[15px] text-[11px] space-y-3">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-2 max-w-sm">
           <Avatar
@@ -30,10 +32,10 @@ const CertContextRow = ({ data }: Props) => {
             <p className="font-bold cursor-default">{data.name}</p>
           )}
         </div>
-        <p className="text-xs font-extralight ml-9 cursor-default text-right">{`Issued ${start_date}`}</p>
       </div>
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-2 justify-between">
         <p className=" text-[11px] font-extralight">{data.authority}</p>
+        <p className="text-xs font-extralight md:ml-9 ml-0 cursor-default text-right">{`Issued ${start_date}`}</p>
       </div>
     </div>
   );
