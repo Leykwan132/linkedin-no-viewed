13a39ac8fab4536d4c48b1eb9286bcc520d364c2
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-06 05:51:03 -0500

    Course mobile version implemented.

--- components/CanvasContent/components/CoursesContentRow.tsx
+++ components/CanvasContent/components/CoursesContentRow.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 const CoursesContentRow = ({ data }: Props) => {
   return (
-    <div className="font-mono flex flex-col text-[15px] space-y-3">
+    <div className="font-mono flex flex-col text-[13px] md:text-[15px] space-y-3">
       <div className="flex items-center justify-between">
-        <p className="font-bold cursor-default">{data.name}</p>
+        <p className="font-bold cursor-default text-left">{data.name}</p>
         <p className="text-xs font-extralight ml-9 cursor-default text-right">
           {data.number}
         </p>
