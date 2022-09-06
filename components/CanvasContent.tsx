3d4cf4747a215d2eab784675a5b58853ce62230e
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-06 06:54:14 -0500

    Icon added for Mobile version.

--- components/CanvasContent.tsx
+++ components/CanvasContent.tsx
@@ -7,6 +7,7 @@ import {
 } from "../atoms/profileAtoms.ts";
 import BioContent from "./CanvasContent/BioContent.tsx";
 import CertContent from "./CanvasContent/CertContent.tsx";
+import PublicationContent from "./CanvasContent/PublicationContent.tsx";
 import CoursesContent from "./CanvasContent/CoursesContent.tsx";
 import EduContent from "./CanvasContent/EduContent.tsx";
 import ExperienceContent from "./CanvasContent/ExperienceContent.tsx";
@@ -14,6 +15,18 @@ import LanguageContent from "./CanvasContent/LanguageContent.tsx";
 import ProjectContent from "./CanvasContent/ProjectContent.tsx";
 import TestScore from "./CanvasContent/TestScoreContent.tsx";
 import VolunteeringContent from "./CanvasContent/VolunteeringContent.tsx";
+import {
+  BookOpenIcon,
+  BeakerIcon,
+  DocumentChartBarIcon,
+  BriefcaseIcon,
+  ChartBarIcon,
+  CubeIcon,
+  DocumentTextIcon,
+  CommandLineIcon,
+  HeartIcon,
+  LanguageIcon,
+} from "@heroicons/react/24/outline";
 
 type Props = {};
 
@@ -30,6 +43,19 @@ interface Content {
   Courses?: any;
   Bio?: any;
 }
+const Icon = {
+  Languages: <LanguageIcon className="w-5 h-5  " />,
+  Education: <BookOpenIcon className="w-5 h-5   " />,
+  Patents: <BeakerIcon className="w-5 h-5  " />,
+  Certs: <DocumentTextIcon className="w-5 h-5  " />,
+  Projects: <ChartBarIcon className="w-5 h-5  " />,
+  Publications: <CommandLineIcon className="w-5 h-5  " />,
+  Courses: <CubeIcon className="w-5 h-5  " />,
+  Volunteering: <HeartIcon className="w-5 h-5  " />,
+  Test_Scores: <DocumentChartBarIcon className="w-5 h-5  " />,
+  Experiences: <BriefcaseIcon className="w-5 h-5  " />,
+};
+
 const contentType: Content = {
   Education: <EduContent />,
   Languages: <LanguageContent />,
@@ -40,6 +66,7 @@ const contentType: Content = {
   Projects: <ProjectContent />,
   Courses: <CoursesContent />,
   Bio: <BioContent />,
+  Publications: <PublicationContent />,
 };
 const CanvasContent = (props: Props) => {
   const canvasData = useRecoilValue(canvasState);
@@ -54,12 +81,14 @@ const CanvasContent = (props: Props) => {
       {isMobile &&
         canvasData !== "Bio" &&
         (canvasData === "Test_Scores" ? (
-          <div className="font-mono text-white text-center font-bold text-xl mb-1">
-            Test Scores
+          <div className="font-mono text-white text-center font-bold text-xl mb-1 flex items-center space-x-2">
+            <TestScore />
+            <p>Test Scores</p>
           </div>
         ) : (
-          <div className="font-mono text-white text-center font-bold text-xl mb-1">
-            {canvasData}
+          <div className="font-mono text-whitefont-bold text-xl mb-2 flex items-center space-x-2 justify-center">
+            <div>{Icon[canvasData]}</div>
+            <div>{canvasData}</div>
           </div>
         ))}
       {contentType[canvasData]}
