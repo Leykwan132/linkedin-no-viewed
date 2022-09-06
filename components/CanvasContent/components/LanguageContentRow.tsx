fb901d65f29befa714bda9ff368a56710964daa8
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-09-06 05:51:40 -0500

    Language mobile version implemented.

--- components/CanvasContent/components/LanguageContentRow.tsx
+++ components/CanvasContent/components/LanguageContentRow.tsx
@@ -1,11 +1,15 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import { useRecoilState } from "recoil";
+import { isMobileState } from "../../../atoms/profileAtoms.ts";
 
 type Props = {
   data?: Object;
 };
 
 const LanguageContentRow = ({ data }: Props) => {
+  const [isMobile, setIsMobile] = useRecoilState(isMobileState);
+
   return (
     <div className="font-mono text-white flex items-center space-x-3">
       <Avatar
@@ -16,7 +20,7 @@ const LanguageContentRow = ({ data }: Props) => {
         }.svg`}
         sx={{ width: 28, height: 28 }}
       />
-      <p>{data}</p>
+      <p className={`${isMobile && "text-sm"}`}>{data}</p>
     </div>
   );
 };
